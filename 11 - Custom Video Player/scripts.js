5bea001e9bcc20cc78ade09f6e82d0ad1342d97a
Pawel <paweljozwiak1986@gmail.com>
2019-01-15 13:25:28 +0100

    day 11 done

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -24,18 +24,40 @@ function updateButton() {
 function skip() {
   video.currentTime += parseFloat(this.dataset.skip);
  }
+ 
+ function handleRangeUpdate() {
+   video[this.name] = this.value;
+ }
+ 
+ function handleProgress() {
+   const percent = (video.currentTime / video.duration) * 100;
+   progressBar.style.flexBasis = `${percent}%`;
+ }
+ 
+ function scrub(e) {
+   const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+   video.currentTime = scrubTime;
+ }
 
 
-function handleRangeUpdate() {
-  video[this.name] = this.value;
-}
-video.addEventListener('click', togglePlay);
-video.addEventListener('play', updateButton);
-video.addEventListener('pause', updateButton);
-video.addEventListener('timeupdate', handleProgress);
+//listeners 
 
-toggle.addEventListener('click', togglePlay);
-skipButtons.forEach(button => button.addEventListener('click', skip));
+ video.addEventListener('click', togglePlay);
+ video.addEventListener('play', updateButton);
+ video.addEventListener('pause', updateButton);
 
-ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
-ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+ video.addEventListener('timeupdate', handleProgress);
+ 
+ toggle.addEventListener('click', togglePlay);
+ skipButtons.forEach(button => button.addEventListener('click', skip));
+ ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+ 
+
+
+ let mousedown = false;
+ progress.addEventListener('click', scrub);
+ progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+ progress.addEventListener('mousedown', () => mousedown = true);
+ progress.addEventListener('mouseup', () => mousedown = false);
+ 
