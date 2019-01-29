0c77b3f2dcd210b24a06bbc5a43289aa67c0e482
Pawel <paweljozwiak1986@gmail.com>
2019-01-29 15:26:36 +0100

    WIP day 19

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -24,17 +24,23 @@ function paintToCanvas(){
   const height = video.videoHeight;
   canvas.width = width;
   canvas.height = height;
+
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+  // take pixels out
+    let pixels = ctx.getImageData(0, 0, width, height);
+  
+  // filters/effects:
+  //pixels = redEffect(pixels);
+  //pixels = rgbSplit(pixels);
+  //ctx.globalAlpha = 0.1;
+  //pixels = greenScreen(pixels);
+  
+  // put pixels back
+    ctx.putImageData(pixels, 0, 0);
+}, 16);
 }
-
-return setInterval(() => {
-  ctx.drawImage(video, 0, 0, width, height);
-  let pixels = ctx.getImageData(0, 0, width, height);
-
-  pixels = redEffect(pixels);
-
-  ctx.putImageData(pixels, 0, 0);
-},16 );
-
 
 function takePhoto() {
   // sound
@@ -56,6 +62,15 @@ function redEffect(pixels) {
     pixels.data[i + 2] = pixels.data[i + 2] + 0.5; // b
   }
   return pixels;
+}
+function rgbSplit(pixels) {
+  for(let i = 0; i < pixels.data.length; i+=4) {
+    pixels.data[i - 150] = pixels.data[i];     // r
+    pixels.data[i + 100] = pixels.data[i + 1]; // g
+    pixels.data[i - 250] = pixels.data[i + 2];  // b
+  }
+  return pixels;
+}
 
 getVideo();
 video.addEventListener("canplay", paintToCanvas);
