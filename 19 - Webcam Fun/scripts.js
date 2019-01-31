289ead6c552d7a0dc27c1737692e69f986aff17f
Pawel <paweljozwiak1986@gmail.com>
2019-01-31 15:55:13 +0100

    WIP day 20

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -32,10 +32,10 @@ function paintToCanvas(){
     let pixels = ctx.getImageData(0, 0, width, height);
   
   // filters/effects:
-  //pixels = redEffect(pixels);
-  //pixels = rgbSplit(pixels);
-  //ctx.globalAlpha = 0.1;
-  //pixels = greenScreen(pixels);
+  pixels = redEffect(pixels);
+  pixels = rgbSplit(pixels);
+  ctx.globalAlpha = 0.1;
+  pixels = greenScreen(pixels);
   
   // put pixels back
     ctx.putImageData(pixels, 0, 0);
@@ -67,10 +67,35 @@ function rgbSplit(pixels) {
   for(let i = 0; i < pixels.data.length; i+=4) {
     pixels.data[i - 150] = pixels.data[i];     // r
     pixels.data[i + 100] = pixels.data[i + 1]; // g
-    pixels.data[i - 250] = pixels.data[i + 2];  // b
+    pixels.data[i - 550] = pixels.data[i + 2];  // b
   }
   return pixels;
 }
+function greenScreen(pixels) {
+  const levels = {};
+
+  document.querySelectorAll('.rgb input').forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (red >= levels.rmin
+      && green >= levels.gmin
+      && blue >= levels.bmin
+      && red <= levels.rmax
+      && green <= levels.gmax
+      && blue <= levels.bmax) {
+      // take it out!
+      pixels.data[i + 3] = 0;
+    }
+  }
+
+  return pixels;
 
 getVideo();
 video.addEventListener("canplay", paintToCanvas);
